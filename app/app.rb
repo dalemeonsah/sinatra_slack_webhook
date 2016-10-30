fdc0d6d0ee143da20e57c0b254c5003abe53eb96
Reinhard Simeon <reinhardsimeon@gmail.com>
2016-10-29 23:16:32 -0700

    add attachments

--- app/app.rb
+++ app/app.rb
@@ -19,3 +19,28 @@ post '/hooks/notify-channel-test' do
 
   message
 end
+
+post '/hooks/notify-channel-test-attachment' do
+  body = JSON.parse(request.body.read)
+  username = body['username']
+  email = body['email']
+  message = "Hello, my username is #{username} and email is #{email}"
+
+  fields = [{ title: "New User Signup", value: message, short: false}]
+  attachments = [attachments('New User', 'good', fields)]
+
+  Slack.to_channel("channel_test", nil, attachments)
+
+  message
+end
+
+# fallback and fields are required fields for message attachments.
+# fallback type is string and fields are an array containing hash objects.
+#
+def attachments(fallback, color=nil, fields)
+  {
+    fallback: fallback,
+    color: color,
+    fields: fields
+  }
+end
--- app/slack.rb
+++ app/slack.rb
@@ -1,5 +1,8 @@
 class Slack
 
+  # As mentioned in Incoming Webhooks section, Slack requires JSON payload and it requires a text field at the minimum.
+  # The username and icon emoji fields are optional and editable at any time.
+  # 
   def self.to_channel(channel, message, attachments=nil)
     RestClient.post(
       'https://hooks.slack.com/services/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', 
